fe67bfe3893723bc5201e9fe96b11b574153444e
Josh Nussbaum <joshnuss@gmail.com>
2024-07-22 23:39:10 -0400

    Adds basic table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-   to read the documentation
-</p>
+<script>
+  import symbols from '../data/symbols.json5'
+  import katex from 'katex'
+</script>
+
+<h1>Math4Devs</h1>
+
+<table>
+  <thead>
+    <tr>
+      <th>Symbol</th>
+      <th>Name</th>
+      <th>Since</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each symbols as symbol}
+      <tr>
+        <td>{symbol.symbol}</td>
+        <td>{symbol.name}</td>
+        <td>{symbol.since}</td>
+        <td>{@html katex.renderToString(symbol.example, { output: 'mathml'})}</td>
+        <td>{symbol.code}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import json5 from 'vite-plugin-json5'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [
+    json5(),
+    sveltekit()
+  ]
 })
